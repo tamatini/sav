bf47d8effbb1303c2eb90f17816e5f6bf4b3edca
Jeffrey Tamatini TEAHUI <45190210+tamatini@users.noreply.github.com>
2019-03-12 00:37:00 -1000

    modification de la requete get
    modification de la requete get sur la base client en fonction du nom.

--- sav_depot/services/client_service.py
+++ sav_depot/services/client_service.py
@@ -9,7 +9,8 @@ client_list = [{'nom': 'TEAHUI', 'prenom': 'Tamatini', 'n°tel': '87 44 44 44'}]
 @api.route('/')
 class ClientList(Resource):
     def get(self):
-       return [{'Nom': c.nom_client, 'Prenom': c.prenom_client, 'Tel': c.tel_client, 'Mail': c.mail_client} for c in Client.query.all()]
+       return [{'Nom': c.nom_client, 'Prénom': c.prenom_client,
+                'Tel': c.tel_client, 'Mail': c.mail_client} for c in Client.query.all()]
 
 
 @api.route('/'+'<string:client_id>')
@@ -17,5 +18,5 @@ class ClientDetail(Resource):
     def get(self, client_id):
         nom = Client.query.filter_by(nom_client=client_id)
         prenom = Client.query.filter_by(prenom_client=client_id)
-        return [{'Nom': c.nom_client,
-                 'Prénom': c.prenom_client} for c in nom] + [{'Nom': c.nom_client, 'Prénom': c.prenom_client} for c in prenom]
+        return [{'Nom': c.nom_client, 'Prénom': c.prenom_client} for c in nom] or \
+               [{'Nom': c.nom_client, 'Prénom': c.prenom_client} for c in prenom]
