ca9d8b743e55a11fe076f73261f2dee8e042713f
tamatini <tamatini@hotmail.fr>
2019-03-30 19:00:13 -1000

    modification de la configuration de démarrage.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # sav_depot
+
+Service de gestion des retours SAV.
+
+Environnement virtuel : 
+
+````bash
+python -m venv VENV
+source VENV/bin/activate
+````
+
+Dépendance : 
+
+````bash
+pip install requirements.txt
+````
+
+Démarrage de l'appli :
+
+````bash
+python3 run.py
+````
+
+ou
+
+````bash
+./run.py
+````
--- run.py
+++ run.py
@@ -1,6 +1,27 @@
-from sav_depot.services import api
-from sav_depot import app
+from sav_depot import create_app
+import argparse
+import os
 
 
-api.init_app(app)
-app.run(debug=True)
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', '-c', help='Path to a config file')
+    return parser.parse_args(argv)
+
+
+def get_full_path(file_path = None):
+    full_path = None
+    if file_path:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        full_path = os.path.join(basedir, file_path)
+    return full_path
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    app = create_app(get_full_path(args.config))
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
